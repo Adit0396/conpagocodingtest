b72880ad00acd9527796860cb93d383f523c915d
Adit0396 <adityaiyer03051996@gmail.com>
2021-08-03 19:59:16 +1000

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { permittedCrossDomainPolicies } from 'helmet';
+import People from './People.js'
+import Button from "./Button"
 function App() {
+  fetch("https://swapi.dev/api/people/1/")
+      .then(res => {res.json();
+      console.log(res)})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <People/>
     </div>
   );
 }
